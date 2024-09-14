73a77cf60e39e6937471201c0d2749083b21af4d
Paulo Vila <paulovila@yahoo.com>
2024-09-13 21:17:09 -0500

    implement initial file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import { Main } from "next/document";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
